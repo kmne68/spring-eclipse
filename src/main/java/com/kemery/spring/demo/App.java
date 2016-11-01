cc8cbdc2b1ed0a8dc5f69f86af08f76364812918
Keith Emery <kmne68@gmail.com>
2016-11-01 13:21:09 -0500

    Add maven dependencies, demo classes.

--- src/main/java/com/kemery/spring/demo/App.java
+++ src/main/java/com/kemery/spring/demo/App.java
@@ -1,10 +1,19 @@
 package com.kemery.spring.demo;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
 public class App {
 
 	public static void main(String[] args) {
 		
+		ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
+		
 		System.out.println("If you see this, it worked.");
+		
+		Person person = (Person)context.getBean("person");
+	//	Person person = new Person();
+		person.speak();
 
 	}
 
---
+++ src/main/java/com/kemery/spring/demo/Person.java
@@ -0,0 +1,9 @@
+package com.kemery.spring.demo;
+
+public class Person {
+
+	public void speak() {
+		
+		System.out.println("I am a person.");
+	}
+}
