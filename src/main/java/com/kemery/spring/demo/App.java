b10f6eff6eaed7379f4af4191a92d24c4ba0b6c0
Keith Emery <kmne68@gmail.com>
2016-11-03 12:41:32 -0500

    Add class path for context

--- src/main/java/com/kemery/spring/demo/App.java
+++ src/main/java/com/kemery/spring/demo/App.java
@@ -1,19 +1,22 @@
 package com.kemery.spring.demo;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class App {
 
 	public static void main(String[] args) {
 		
-		ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
-		
-		System.out.println("If you see this, it worked.");
+//		ApplicationContext context = new FileSystemXmlApplicationContext("src/main/java/com/kemery/spring/demo/beans/beans.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/kemery/spring/demo/beans/beans.xml");
+	
 		
 		Person person = (Person)context.getBean("person");
 	//	Person person = new Person();
 		person.speak();
+		
+		((ClassPathXmlApplicationContext)context).close();
 
 	}
 
