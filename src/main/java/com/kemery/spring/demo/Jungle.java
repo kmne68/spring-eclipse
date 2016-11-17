4efc4458aa329ee8634b2529bd93993f5c61e178
Keith Emery <kmne68@gmail.com>
2016-11-17 13:52:52 -0600

    Add key-value pairs with Objects as values

--- src/main/java/com/kemery/spring/demo/Jungle.java
+++ src/main/java/com/kemery/spring/demo/Jungle.java
@@ -9,8 +9,9 @@ public class Jungle {
 
 	private Animal largest;
 	private Animal monkeyest;
-	private List<Animal> animals;
+	private List<Animal> beasts;
 	private Map<String, String> foods = new HashMap<String, String>();
+	private Map<String, Animal> animals = new HashMap<String, Animal>();
 	
 	
 	public void setFoods(Map<String, String> foods) {
@@ -45,11 +46,17 @@ public class Jungle {
 	
 	public List<Animal> getAnimals() {
 		
-		return animals;
+		return beasts;
 	}
 
 	
-	public void setAnimals(List<Animal> animals) {
+	public void setBeasts(List<Animal> animals) {
+		
+		this.beasts = animals;
+	}
+	
+	
+	public void setAnimals(Map<String, Animal> animals) {
 		
 		this.animals = animals;
 	}
@@ -65,12 +72,17 @@ public class Jungle {
 		sb.append("Monkeyest: " + monkeyest);
 		sb.append("Others:\n");
 		
-		for(Animal animal: animals) {
+		for(Animal animal: beasts) {
 			sb.append(animal);
 			sb.append("\n");
 		}
 		
 		for(Map.Entry<String, String> entry: foods.entrySet()) {
+			
+			sb.append(entry.getKey() + ": " + entry.getValue() + "\n");
+		}
+		
+		for(Map.Entry<String, Animal> entry: animals.entrySet()) {
 			
 			sb.append(entry.getKey() + ": " + entry.getValue() + "\n");
 		}
