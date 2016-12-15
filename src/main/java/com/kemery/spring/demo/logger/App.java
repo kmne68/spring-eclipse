1ccb907207605929d3c03f8c856f702b04eb706c
Keith Emery <kmne68@gmail.com>
2016-12-14 19:11:01 -0600

    Add query (WIP-no bean found)

--- src/main/java/com/kemery/spring/demo/logger/App.java
+++ src/main/java/com/kemery/spring/demo/logger/App.java
@@ -1,9 +1,13 @@
 package com.kemery.spring.demo.logger;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.kemery.spring.demo.Robot;
+import com.kemery.spring.demo.model.Offer;
+import com.kemery.spring.demo.model.OffersDAO;
 
 public class App {
 
@@ -13,14 +17,22 @@ public class App {
 		
 		// 		ApplicationContext context = new ClassPathXmlApplicationContext("com/kemery/spring/demo/beans/beans.xml");
 		
-		Logger logger = (Logger)context.getBean("logger");
+		// Logger logger = (Logger)context.getBean("logger");
 		
-		Robot robot = (Robot)context.getBean("robot");
+		// Robot robot = (Robot)context.getBean("robot");
 		
-		logger.writeConsole("Hello there");
-		logger.writeFile("Hi again");
+		OffersDAO offersDao = (OffersDAO)context.getBean("offersDao");
 		
-		robot.speak();
+		List<Offer> offers = offersDao.getOffers();
+		
+		for(Offer offer: offers) {
+			System.out.println(offer);
+		}
+		
+		//logger.writeConsole("Hello there");
+		//logger.writeFile("Hi again");
+		
+		// robot.speak();
 		
 		((ClassPathXmlApplicationContext)context).close();
 	}
